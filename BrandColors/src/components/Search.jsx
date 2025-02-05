196334d9ae0c26dc982560fd32acaab4f40ebaa8
yunus kupucu <yunus.kupucu@gmail.com>
2025-02-06 00:54:28 +0300

    searchbar created

--- BrandColors/src/components/Search.jsx
+++ BrandColors/src/components/Search.jsx
@@ -1,13 +1,20 @@
 import { GrSearch } from 'react-icons/gr';
+import MainContext from '../MainContext';
+import { useContext } from 'react';
 
 const Search = () => {
+  const { search, setSearch } = useContext(MainContext);
   return (
     <div>
       <div className="search">
         <div className="icon">
           <GrSearch />
         </div>
-        <input type="text" placeholder="Search Brands" />
+        <input
+          type="text"
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search Brands"
+        />
       </div>
     </div>
   );
