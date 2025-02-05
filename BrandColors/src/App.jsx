949e5e05ad7e835c2553e5069ca15f9bc7439bc3
yunus kupucu <yunus.kupucu@gmail.com>
2025-02-06 00:28:24 +0300

    add router package

--- BrandColors/src/App.jsx
+++ BrandColors/src/App.jsx
@@ -4,6 +4,8 @@ import Sidebar from './components/Sidebar';
 import Content from './components/Content';
 import BrandsData from './brands.json';
 import { useEffect, useState } from 'react';
+import Copied from './components/Copied';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   const brandsArray = [];
@@ -13,16 +15,40 @@ function App() {
 
   const [brands, setBrands] = useState(brandsArray);
   const [selectedBrands, setSelectedBrands] = useState([]);
+  const [copied, setCopied] = useState(false);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     console.log(selectedBrands);
   }, [selectedBrands]);
 
-  const data = { brands, selectedBrands, setSelectedBrands };
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCopied(false);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [copied]);
+
+  useEffect(() => {
+    setBrands(
+      brandsArray.filter((brand) => brand.title.toLowerCase().includes(search))
+    );
+  }, [search]);
+
+  const data = {
+    brands,
+    selectedBrands,
+    setSelectedBrands,
+    setCopied,
+    search,
+    setSearch,
+  };
 
   return (
     <>
       <MainContext.Provider value={data}>
+        {copied && <Copied color={copied} />}
         <Sidebar />
         <Content />
       </MainContext.Provider>
