7bf17f750968966c3ed070694a0878ab0369bdfd
yunus kupucu <yunus.kupucu@gmail.com>
2025-02-07 01:37:27 +0300

    redux implementing

--- redux-intro/src/main.jsx
+++ redux-intro/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import configureStore from './redux/reducers/configureStore';
+import { Provider } from 'react-redux';
+
+const store = configureStore();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
