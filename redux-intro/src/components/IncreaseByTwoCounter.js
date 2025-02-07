e80cfa88ae27f1aba49b2c864e0e690d5c77efbc
yunus kupucu <yunus.kupucu@gmail.com>
2025-02-08 01:58:26 +0300

    components created

---
+++ redux-intro/src/components/IncreaseByTwoCounter.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class IncreaseByTwoCounter extends Component {
+  render() {
+    return <div></div>;
+  }
+}
---
+++ redux-intro/src/components/IncreaseCounter.js
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+
+export default class IncreaseCounter extends Component {
+  render() {
+    return <div></div>;
+  }
+}
