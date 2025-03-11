d3896cbf5ba42e56516ae8337d4a1d04821eb94c
yunus kupucu <yunus.kupucu@gmail.com>
2025-03-11 03:23:07 +0300

    initialize dark mode feature with React and Vite setup

---
+++ dark-mode/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import '../src/index.css';
+import DarkMode from './assets/component/darkMode';
+
+function App() {
+  return (
+    <div>
+      <DarkMode />
+    </div>
+  );
+}
+
+export default App;
